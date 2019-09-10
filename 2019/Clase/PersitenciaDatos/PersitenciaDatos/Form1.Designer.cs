4de4ba4cfb9379299e2b6934376c1984fb8d1239
Sergio <sergioalgorry@hotmail.com>
2019-09-10 18:20:39 -0300

    Programa de ejercitacion de uso de tablas

---
+++ 2019/Clase/PersitenciaDatos/PersitenciaDatos/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace PersitenciaDatos
+{
+    partial class frmABM
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvPersonas = new System.Windows.Forms.DataGridView();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.btGrabar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 27);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(206, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Domicilio";
+            // 
+            // txtDomicilio
+            // 
+            this.txtDomicilio.Location = new System.Drawing.Point(224, 27);
+            this.txtDomicilio.Name = "txtDomicilio";
+            this.txtDomicilio.Size = new System.Drawing.Size(320, 20);
+            this.txtDomicilio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(557, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(550, 27);
+            this.txtDNI.MaxLength = 8;
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(101, 20);
+            this.txtDNI.TabIndex = 5;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(657, 27);
+            this.txtEdad.MaxLength = 3;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(35, 20);
+            this.txtEdad.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(657, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edad";
+            // 
+            // dgvPersonas
+            // 
+            this.dgvPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonas.Location = new System.Drawing.Point(12, 93);
+            this.dgvPersonas.Name = "dgvPersonas";
+            this.dgvPersonas.Size = new System.Drawing.Size(680, 283);
+            this.dgvPersonas.TabIndex = 8;
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAceptar.Location = new System.Drawing.Point(550, 53);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(142, 33);
+            this.btAceptar.TabIndex = 9;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.BtAceptar_Click);
+            // 
+            // btGrabar
+            // 
+            this.btGrabar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGrabar.Location = new System.Drawing.Point(402, 54);
+            this.btGrabar.Name = "btGrabar";
+            this.btGrabar.Size = new System.Drawing.Size(142, 33);
+            this.btGrabar.TabIndex = 10;
+            this.btGrabar.Text = "&Grabar";
+            this.btGrabar.UseVisualStyleBackColor = true;
+            this.btGrabar.Click += new System.EventHandler(this.BtGrabar_Click);
+            // 
+            // frmABM
+            // 
+            this.AcceptButton = this.btAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 391);
+            this.Controls.Add(this.btGrabar);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.dgvPersonas);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDomicilio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "frmABM";
+            this.Text = "ABM datos de Personas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDomicilio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvPersonas;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Button btGrabar;
+    }
+}
+
---
+++ 2019/Clase/PersitenciaDatos/PersitenciaDatos/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PersitenciaDatos
+{
+    public partial class frmABM : Form
+    {
+        #region PROPIEDADES PRIVADAS
+        /// <summary>
+        /// Path y nombre del archivo
+        /// </summary>
+        private const string NombreArchivo = @"personas.xml";
+
+        /// <summary>
+        /// Tabla en memoria con la lista de personas
+        /// </summary>
+        private System.Data.DataTable TablaPersona = new DataTable("TablaPersonas");
+        #endregion
+
+        #region CONSTRUCTOR
+        public frmABM()
+        {
+            InitializeComponent();
+
+            ConfiguracionInicial();
+
+        }
+        #endregion
+
+        #region EVENTOS
+        private void BtAceptar_Click(object sender, EventArgs e)
+        {
+            // Agrega registro nuevo a la tabla
+            TablaPersona.Rows.Add();
+
+            LlenaRegistroTabla();
+
+            LimpiarCampos();
+
+            txtNombre.Focus();
+        }
+
+        private void BtGrabar_Click(object sender, EventArgs e)
+        {
+            TablaPersona.WriteXml(NombreArchivo);
+        }
+        #endregion
+
+        #region METODOS
+
+        /// <summary>
+        /// Diseña columnas del datatable, 
+        /// verifica si el archivo existe previamente
+        /// asigna tabla al DataGridView
+        /// </summary>
+        private void ConfiguracionInicial()
+        {
+            // diseño de la TablaPersona
+            TablaPersona.Columns.Add("Nombre");
+            TablaPersona.Columns.Add("Domicilio");
+            TablaPersona.Columns.Add("DNI");
+            TablaPersona.Columns.Add("Edad");
+
+            // busca si el archivo ya existe para precargar sus datos
+            if (System.IO.File.Exists(NombreArchivo))
+            {
+                TablaPersona.ReadXml(NombreArchivo);
+            }
+
+            // asigna TablaPersona a dgvPersonas
+            dgvPersonas.DataSource = TablaPersona;
+            // elimina la posibilidad de ingresar datos en el datagridview
+            dgvPersonas.AllowUserToAddRows = false;
+            // elimina el encabezado de las filas
+            dgvPersonas.RowHeadersVisible=false;
+        }
+
+        // Llena con string vacio los texbox
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtDomicilio.Text = "";
+            txtEdad.Text = "";
+            txtDNI.Text = "";
+        }
+
+        /// <summary>
+        /// Rellena las columnas del último registro agregado con los valores del textbox correspondiente
+        /// </summary>
+        private void LlenaRegistroTabla()
+        {
+            TablaPersona.Rows[TablaPersona.Rows.Count - 1]["Nombre"] = txtNombre.Text;
+            TablaPersona.Rows[TablaPersona.Rows.Count - 1]["Domicilio"] = txtDomicilio.Text;
+            TablaPersona.Rows[TablaPersona.Rows.Count - 1]["DNI"] = txtDNI.Text;
+            TablaPersona.Rows[TablaPersona.Rows.Count - 1]["Edad"] = txtEdad.Text;
+        }
+        #endregion
+    }
+}
